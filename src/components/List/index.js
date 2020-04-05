d451c9f4a95b9de03d304f1c11109a64fe22f263
Adam Abundis <adamjabundis@gmail.com>
2020-04-05 16:18:59 -0700

    Move 'Item' logic to a seperate Item component.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import Item from '../Item';
 
-const List = props => {
-    return props.list.map(item => (
-        <div key={item.objectID}>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-        </div>
+const List = ({ list }) => {
+    return list.map(item => (
+      <Item key={item.objectID} item={item}/>
     ));
 }
 
