d4e52573c12cb426db84f5e6c09fd34ded45f99c
Adam Abundis <adamjabundis@gmail.com>
2020-04-05 16:18:07 -0700

    move the handleChange function to 'App'

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-const Search = () => {
-  const handleChange = event => {
-    console.log(event.target.value);
-  }
-  
+const Search = ({search, onSearch}) => {
   return (
     <section>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+      <input id="search" type="text" value={search} onChange={onSearch} />
     </section>
   );
 }
