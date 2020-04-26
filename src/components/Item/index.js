dd54f9657b8cc4540eb0420a858f18d991637c33
Adam Abundis <adamjabundis@gmail.com>
2020-04-26 00:17:55 -0700

    Item - add delete func that references App.js func

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -8,7 +8,11 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
-
 export default Item;
